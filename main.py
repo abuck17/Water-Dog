41dc025b126e205d1f7c7ba64063badd429f5043
Andrew Buckley <abuck17@gmail.com>
2023-12-21 21:59:51 -0500

    Init

--- main.py
+++ main.py
@@ -1,62 +1,16 @@
-import json
-#import requests
-import threading
 import time
-#from RPi import GPIO
-
-# TODO: No longer needing test data
-
-test_data = {}
-lock = threading.Lock()
-
-def update_test_data():
-	global test_data
-	while True:
-		with open("test.json", "r") as file:
-			try:
-				new_test_data = json.load(file)
-			except json.JSONDecodeError:
-				continue
-		
-		with lock:
-			test_data = new_test_data
-
-		time.sleep(1)
-		
-thread = threading.Thread(target=update_test_data)
-thread.daemon = True
-thread.start()
-
-time.sleep(5)
-print("Using Test Data")
+import requests
+from RPi import GPIO
 
 # Constants
 
-relay_pin        = 1
-float_switch_pin = 2
-flow_sensor_pin  = 3
-
-min_flow_rate = 1   #  1 l/min
-max_flow_rate = 60  # 60 l/min
-
-relay_timeout = 5.0 * 60.0  # 5 mins
-
-secs_to_mins = 1/60
+float_switch_pin = 1
 
 class State():
 
 	On = True
 	Off = False
 
-class Relay():
-
-	def __init__(self, pin):
-		self.__pin = pin
-		self.__state = State.Off
-		
-	def set_state(self, state):
-		self.state = state
-		
 class Float_Switch():
 
 	def __init__(self, pin):
@@ -64,59 +18,36 @@ class Float_Switch():
 		self.__state = self.get_state()
 		
 	def get_state(self):
-		# TODO: Update with correct "GET"
-		return test_data["FLOAT_SWITCH_STATE"] == "ON"
+		pass
 		
-		
-class Flow_Sensor():
+class Notification():
 
-	def __init__(self, pin):
-		self.__pin = pin
-		self.__rate = self.get_flow_rate()
+	def __init__(self):
+		pass
 		
-	def get_flow_rate(self):
-		# TODO: Update with correct "GET"
-		return float(test_data["FLOW_SENSOR_FLOW_RATE"])
-
+	def send(title, message):
+		pass
+		
 def main():
 
-	relay = Relay(relay_pin)
+	notification = Notification()
 	float_switch = Float_Switch(float_switch_pin)
-	flow_sensor = Flow_Sensor(flow_sensor_pin)
 	
 	while True:
 		
 		if float_switch.get_state() == State.On:
 		
-			print("Notify Pump Was Turned On")
-			relay.set_state(State.On)
 			
-			litters_pumped = 0.0
-			relay_start_time = time.time()
-			relay_previous_time = relay_start_time
-			
+			notification.send("Water Dog", "Float Switch Has Been Triggered")
+	
 			while True:
 			
-				relay_current_time = time.time()
-				
-				time_delta = relay_current_time - relay_previous_time
-								
-				flow_rate = flow_sensor.get_flow_rate()
-				
-				litters_pumped += flow_rate * time_delta * secs_to_mins
-							
-				if relay_current_time > relay_start_time + relay_timeout:
-					print("Notify Pump Was Turned Off By Timeout")
-					break
-			
-				if float_switch.get_state() == State.Off and flow_rate <= min_flow_rate:
-					print("Notify Pump Was Turned Off By Float Switch And Flow Sensor")
+				if float_switch.get_state() == State.Off:
 					break
 					
-				relay_previous_time = relay_current_time
+				time.sleep(1.0)
+		time.sleep(1.0)
 			
-			print("Notify %f Litters Of Water Was Pumped" % litters_pumped)
-			relay.set_state(State.Off)
 
 if __name__ == "__main__":
 	main()
